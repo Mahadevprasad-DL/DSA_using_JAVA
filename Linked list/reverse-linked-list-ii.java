942462afa0f0939719a015d1079b818fa5847737
Mahadevprasad D L <mahadevprasaddl@gmail.com>
2025-08-23 13:14:52 +0530

    Linked list problems

---
+++ Linked list/reverse-linked-list-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if ( head == null || left == right) return head;
+
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        ListNode prev = temp;
+
+        for(int i=0; i<left-1; i++){
+            prev = prev.next;
+        }
+
+        ListNode curr = prev.next;
+
+        for(int i=0; i<right-left; i++){
+            ListNode next =curr.next;
+            curr.next = next.next;
+            next.next = prev.next;
+            prev.next= next;
+        }
+
+        return temp.next;
+
+
+        
+    }
+}
