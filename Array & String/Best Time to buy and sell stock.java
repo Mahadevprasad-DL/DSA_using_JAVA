e1230100ac91dbdb59b9c5d4c562b5f4a263f170
Mahadevprasad D L <mahadevprasaddl@gmail.com>
2025-08-30 00:02:05 +0530

    Update Best Time to buy and sell stock.java

--- Array & String/Best Time to buy and sell stock.java
+++ Array & String/Best Time to buy and sell stock.java
@@ -1,25 +1,18 @@
 class Solution {
-    public static void main(String[] args){
-        Solution sl = new Solution();
-        int prices[] ={7,1,5,3,6,4};
-        System.out.println(sl.maxProfit(prices));
-    }
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int max_Profit=0;
-        int buy_price=prices[0];
-        for(int i=0; i<n; i++)
-        {
-            int curr_profit=prices[i]-buy_price;
-            if(curr_profit > max_Profit)
-            {
-                max_Profit = curr_profit;
-            }
-            if(prices[i]<buy_price)
-            {
-                buy_price=prices[i];
+        int buyprice = prices[0];
+        int profit =0;
+
+        for(int i=0; i<prices.length; i++){
+            if(prices[i] < buyprice){
+                buyprice = prices[i];
+            } else {
+                int current_profit = prices[i]-buyprice;
+                profit = Math.max(current_profit, profit);
             }
         }
-        return max_Profit;
+
+        return profit;
+        
     }
 }
